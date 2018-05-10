94aff23c9c2a6e867a4491ea2ed93fb44847f409
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-10 21:17:59 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns = 0
   board.each do |spot|
-    if spot == "X" || spot == "O" then turns += 1end
+    if spot == "X" || spot == "O" then turns += 1 end
   end
   turns
 end
